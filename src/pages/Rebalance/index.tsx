e9003b626165786168877c056972a440706a7eec
mayconline <mayconline.ti@gmail.com>
2020-07-29 22:16:57 -0300

    create rebalance layout

--- src/pages/Rebalance/index.tsx
+++ src/pages/Rebalance/index.tsx
@@ -1,13 +1,154 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { FlatList } from 'react-native';
+import { ThemeContext } from 'styled-components/native';
 
-import { Wrapper } from './styles';
+import {
+  Wrapper,
+  SubHeader,
+  List,
+  Content,
+  Title,
+  FiltersContainer,
+  Filter,
+  TextFilter,
+  Card,
+  CardContent,
+  CardTitle,
+  SubTitleContant,
+  CardSubTitle,
+  CurrentPercent,
+  TargetPercent,
+  ProgressBar,
+  AmountContainer,
+  Amount,
+  Status,
+} from './styles';
+
+import { FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons';
 
 import Header from '../../components/Header';
+import { formatNumber, formatStatus } from '../../utils/format';
 
-const Rebalance: React.FC = () => (
-  <Wrapper>
-    <Header title="Rebalancear Ativos" />
-  </Wrapper>
-);
+const CARD_LIST = [
+  {
+    title: 'Lojas Renner',
+    ticket: 'LREN3',
+    subTitle: '+40.5%',
+    amount: 40070.2454,
+    status: 'BUY',
+    financialCurrency: 'BRL',
+    percent: 0.6,
+  },
+  {
+    title: 'Transmissão Paulista',
+    ticket: 'TRPL4',
+    subTitle: '+10.5%',
+    amount: 10.2454,
+    status: 'BUY',
+    financialCurrency: 'BRL',
+    percent: 0.85,
+  },
+  {
+    title: 'Engie',
+    ticket: 'EGIE3',
+    subTitle: '+0.5%',
+    amount: 210.2454,
+    status: 'BUY',
+    financialCurrency: 'BRL',
+    percent: 0.95,
+  },
+  {
+    title: 'Cshg Logistica',
+    ticket: 'HGLG11',
+    subTitle: '+50.5%',
+    amount: 400.2454,
+    status: 'BUY',
+    financialCurrency: 'BRL',
+    percent: 0.45,
+  },
+  {
+    title: 'Alphabet Inc.',
+    ticket: 'GOOG',
+    subTitle: '+0%',
+    amount: 0,
+    status: 'KEEP',
+    financialCurrency: 'USD',
+    percent: 1,
+  },
+  {
+    title: 'Magazine Luiza SA.',
+    ticket: 'MGLU3',
+    subTitle: '-2.3%',
+    amount: -300.78124,
+    status: 'ANALIZE',
+    financialCurrency: 'BRL',
+    percent: 1,
+  },
+];
+
+const Rebalance: React.FC = () => {
+  const { color } = useContext(ThemeContext);
+
+  return (
+    <Wrapper>
+      <Header />
+      <SubHeader>
+        <Title>Rebalancear</Title>
+        <FiltersContainer>
+          <Filter>
+            <TextFilter focused={false}>Ativo</TextFilter>
+          </Filter>
+          <Filter>
+            <TextFilter focused={true}>Quantidade</TextFilter>
+          </Filter>
+          <FontAwesome5
+            name="sort-amount-up"
+            size={24}
+            color={color.subtitle}
+          />
+        </FiltersContainer>
+      </SubHeader>
+      <List>
+        <FlatList
+          data={CARD_LIST}
+          keyExtractor={item => item.ticket}
+          renderItem={({ item }) => (
+            <Content>
+              <Card>
+                <CardContent>
+                  <CardTitle>
+                    {item.title} ({item.ticket})
+                  </CardTitle>
+                  <SubTitleContant>
+                    <CardSubTitle>
+                      <CurrentPercent>% Atual</CurrentPercent>
+                      <TargetPercent status={item.status}>
+                        {item.subTitle}
+                      </TargetPercent>
+                    </CardSubTitle>
+                    <ProgressBar
+                      styleAttr="Horizontal"
+                      indeterminate={false}
+                      progress={item.percent}
+                      color={color.blue}
+                    />
+                  </SubTitleContant>
+                </CardContent>
+                <AmountContainer>
+                  <Amount status={item.status}>
+                    {formatNumber(item.amount, item.financialCurrency)}
+                  </Amount>
+                  <Status status={item.status}>
+                    {formatStatus(item.status)}
+                  </Status>
+                </AmountContainer>
+              </Card>
+            </Content>
+          )}
+        />
+      </List>
+    </Wrapper>
+  );
+};
 
 export default Rebalance;
--- src/pages/Rebalance/styles.ts
+++ src/pages/Rebalance/styles.ts
@@ -1,6 +1,147 @@
 import styled from 'styled-components/native';
 
+interface filtersProps {
+  onPress?(): void;
+  focused?: boolean;
+  status?: string;
+}
+
 export const Wrapper = styled.View`
   flex: 1;
+  background-color: ${({ theme }) => theme.color.primary};
+`;
+
+export const SubHeader = styled.SafeAreaView`
   background-color: ${({ theme }) => theme.color.secondary};
+  padding: 20px 20px 4px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.color.title};
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 600;
+  font-family: 'TitilliumWeb_600SemiBold';
+`;
+
+export const FiltersContainer = styled.View`
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  margin: 8px 0;
+`;
+
+export const Filter = styled.TouchableOpacity`
+  margin-right: 8px;
+`;
+
+export const TextFilter = styled.Text<filtersProps>`
+  background-color: ${({ focused, theme }) =>
+    focused ? theme.color.bgFiltersActive : theme.color.secondary};
+  color: ${({ focused, theme }) =>
+    focused ? theme.color.blue : theme.color.subtitle};
+  padding: 4px 16px;
+  border-radius: 16px;
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 600;
+  font-family: 'TitilliumWeb_600SemiBold';
+`;
+
+export const List = styled.SafeAreaView`
+  background-color: ${({ theme }) => theme.color.secondary};
+  flex: 1;
+`;
+
+export const Content = styled.SafeAreaView`
+  background-color: ${({ theme }) => theme.color.secondary};
+  padding: 4px 12px 0;
+  flex: 1;
+`;
+
+export const Card = styled.View`
+  background-color: ${({ theme }) => theme.color.blueLight};
+  flex: 1;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: row;
+  margin: 4px 0;
+  border-radius: 30px;
+  padding: 8px 20px;
+`;
+export const CardContent = styled.View`
+  flex: 1;
+`;
+export const CardTitle = styled.Text`
+  color: ${({ theme }) => theme.color.title};
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 600;
+  font-family: 'TitilliumWeb_600SemiBold';
+`;
+
+export const SubTitleContant = styled.View`
+  margin: 0 8px 0 0;
+`;
+
+export const CardSubTitle = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  margin: 4px 0 -8px;
+`;
+
+export const CurrentPercent = styled.Text`
+  color: ${({ theme }) => theme.color.subtitle};
+  font-size: 12px;
+  line-height: 16px;
+  font-family: 'TitilliumWeb_600SemiBold';
+`;
+export const TargetPercent = styled.Text<filtersProps>`
+  color: ${({ theme, status }) =>
+    status === 'BUY'
+      ? theme.color.success
+      : status === 'KEEP'
+      ? theme.color.warning
+      : theme.color.danger};
+  font-size: 12px;
+  line-height: 16px;
+  font-family: 'TitilliumWeb_600SemiBold';
+`;
+
+export const ProgressBar = styled.ProgressBarAndroid``;
+
+export const AmountContainer = styled.View`
+  align-items: center;
+  width: 35%;
+  margin-right: -12px;
+`;
+
+export const Amount = styled.Text<filtersProps>`
+  color: ${({ theme, status }) =>
+    status === 'BUY'
+      ? theme.color.success
+      : status === 'KEEP'
+      ? theme.color.warning
+      : theme.color.danger};
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 600;
+  font-family: 'TitilliumWeb_600SemiBold';
+`;
+
+export const Status = styled.Text<filtersProps>`
+  color: ${({ theme, status }) =>
+    status === 'BUY'
+      ? theme.color.success
+      : status === 'KEEP'
+      ? theme.color.warning
+      : theme.color.danger};
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 600;
+  font-family: 'TitilliumWeb_600SemiBold';
 `;
