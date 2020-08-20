54fa4d0e20abf991992f08a9e614de28d528d692
mayconline <mayconline.ti@gmail.com>
2020-08-20 20:21:37 -0300

    initial add ticket form

--- src/pages/AddTicket/index.tsx
+++ src/pages/AddTicket/index.tsx
@@ -1,6 +1,82 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { ThemeContext } from 'styled-components/native';
 
-const AddTicket: React.FC = () => <Text>Add ticket</Text>;
+import { Wrapper, Form, FormRow, InputGroup, Label, Input } from './styles';
+import Header from '../../components/Header';
+
+interface ITicketForm {
+  ticket: string;
+  grade: string;
+  quantity: string;
+  averagePrice: string;
+}
+
+const AddTicket: React.FC = () => {
+  const { color } = useContext(ThemeContext);
+  const [ticketForm, setTicketForm] = useState<ITicketForm>({} as ITicketForm);
+
+  return (
+    <Wrapper>
+      <Header />
+      <Form>
+        <FormRow>
+          <InputGroup>
+            <Label>Ativo</Label>
+            <Input
+              value={ticketForm.ticket}
+              placeholder="Busque o ativo"
+              placeholderTextColor={color.titleNotImport}
+              maxLength={6}
+              onChangeText={ticket =>
+                setTicketForm(ticketForm => ({ ...ticketForm, ticket }))
+              }
+              onEndEditing={e => console.log(ticketForm.ticket)}
+            />
+          </InputGroup>
+          <InputGroup>
+            <Label>Nota</Label>
+            <Input
+              value={ticketForm.grade}
+              keyboardType="number-pad"
+              placeholder="Dê uma nota"
+              placeholderTextColor={color.titleNotImport}
+              maxLength={3}
+              onChangeText={grade =>
+                setTicketForm(ticketForm => ({ ...ticketForm, grade }))
+              }
+            />
+          </InputGroup>
+        </FormRow>
+        <FormRow>
+          <InputGroup>
+            <Label>Quantidade</Label>
+            <Input
+              value={ticketForm.quantity}
+              keyboardType="number-pad"
+              placeholder="Números de ativos"
+              placeholderTextColor={color.titleNotImport}
+              onChangeText={quantity =>
+                setTicketForm(ticketForm => ({ ...ticketForm, quantity }))
+              }
+            />
+          </InputGroup>
+          <InputGroup>
+            <Label>Preço Médio</Label>
+            <Input
+              value={ticketForm.averagePrice}
+              keyboardType="number-pad"
+              placeholder="Preço médio do ativo"
+              placeholderTextColor={color.titleNotImport}
+              onChangeText={averagePrice =>
+                setTicketForm(ticketForm => ({ ...ticketForm, averagePrice }))
+              }
+              onEndEditing={e => console.log(ticketForm)}
+            />
+          </InputGroup>
+        </FormRow>
+      </Form>
+    </Wrapper>
+  );
+};
 
 export default AddTicket;
---
+++ src/pages/AddTicket/styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components/native';
+
+export const Wrapper = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.color.primary};
+`;
+
+export const Form = styled.SafeAreaView`
+  background-color: ${({ theme }) => theme.color.secondary};
+  padding: 20px 20px 4px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  flex: 1;
+`;
+
+export const FormRow = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 16px;
+`;
+
+export const InputGroup = styled.View``;
+
+export const Label = styled.Text`
+  color: ${({ theme }) => theme.color.titleNotImport};
+  font: 600 16px/24px 'TitilliumWeb_600SemiBold';
+`;
+export const Input = styled.TextInput`
+  color: ${({ theme }) => theme.color.title};
+  height: 32px;
+  padding: 0 8px;
+  font: 400 16px/24px 'TitilliumWeb_400Regular';
+`;
